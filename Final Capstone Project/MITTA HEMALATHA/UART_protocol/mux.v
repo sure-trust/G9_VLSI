d940bcb242d0da4156d3347b02f20ad09fe1cde5
Hema-m-01 <140885541+Hema-m-01@users.noreply.github.com>
2023-09-03 21:29:21 +0530

    Final project submitted

---
+++ Final Capstone Project/MITTA HEMALATHA/UART_protocol/mux.v
@@ -0,0 +1,13 @@
+module mux(
+input i_sel_1,i_sel_0,i_data_0,i_data_1,i_data_2,i_data_3,output reg o_data);
+always@(*)
+begin
+	case({i_sel_1,i_sel_0})
+	2'b00:o_data=i_data_0;
+	2'b01:o_data=i_data_1;
+	2'b10:o_data=i_data_2;
+	2'b11:o_data=i_data_3;
+	//default:o_data=1'b0;
+	endcase
+	end
+endmodule
